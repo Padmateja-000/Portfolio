371bcbd9da16ccc88abc396f9ca2cd4ea3dab487
Padmateja-000 <trypadmateja@gmail.com>
2026-03-20 12:28:03 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-echo "Building the project..."
-python3.12 -m pip install -r requirements.txt
+pip install -r requirements.txt
 
-echo "Make Migrations..."
-python3.12 manage.py makemigrations --noinput
-python3.12 manage.py migrate --noinput
-
-echo "Collect Static..."
-python3.12 manage.py collectstatic --noinput --clear
+python manage.py migrate
+python manage.py collectstatic --noinput
